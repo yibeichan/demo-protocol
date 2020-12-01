26de53f7971791ebf6dca25d44e38150ecd36762
Sanu Ann <sanuann@mit.edu>
2020-12-01 14:23:05 -0500

    change back banner message

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/VoicePilot/VoicePilot_schema',
-  banner: 'This is a demonstration protocol for ReproSchema. Test',
+  banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
   backendServer: null
