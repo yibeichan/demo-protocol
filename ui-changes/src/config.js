3caf96607f7392b4283985237e497cf508e5ff2c
Frances <40801017+MulliganF02@users.noreply.github.com>
2021-02-11 11:08:26 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/VoicePilot/VoicePilot_schema',
+  githubSrc: 'https://raw.githubusercontent.com/MulliganF02/demo-protocol/master/demo-protocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
