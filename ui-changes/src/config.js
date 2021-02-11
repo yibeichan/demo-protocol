d4221b92b51d0483e0759c51ae2e0145d5f87fce
Frances <40801017+MulliganF02@users.noreply.github.com>
2021-02-11 11:16:30 -0500

    fixing issues

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/VoicePilot/VoicePilot_schema',
+  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/' +
+      "activities/activity1",
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
