aca28c956957b1b61530a44cfa8c03025be414d7
Frances <40801017+MulliganF02@users.noreply.github.com>
2021-02-26 10:56:55 -0500

    fixed local testing temporarily

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/MulliganF02/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
